5aafa2902eed571e89939b17a4a0e3e6df135f9f
shuji <bh2@qq.com>
2018-04-11 18:46:23 +0800

    update

---
+++ README.md
@@ -0,0 +1,56 @@
+Used like [torndb](https://github.com/bdarnell/torndb) .
+
+
+zookeeper of hs2 is supported by default.
+
+This project provides three methods: `db.query`, `db.get`, `db.execute`.
+
+`db.query` and `db.get` are used like torndb.
+
+Only `db.execute` will return None, while `torndb.execute` returns lastrowid.
+
+
+Please read the source code, it's very simple.
+
+
+封装一下 impyla，让其使用起来和 torndb 一样简单好用。
+
+提供 db.query、db.get、db.execute 三个方法，用法和 torndb 完全一样。
+
+query 和 get 返回值数据类型也完全一样; 惟一不同的是 execute 方法，torndb 中会返回 lastrowid，本文中返回 None
+
+请仔细阅读源码，了解使用方法，不用担心，源码不到 100 行。
+
+
+### Install
+
+    $ pipenv install
+
+If you CAN NOT use the sasl C lib, you can use the pure-sasl python lib,
+
+else please install sasl by yourself.
+
+    $ pipenv install sasl
+
+
+And if some sasl error occurs, please uninstall sasl:
+
+    $ pipenv uninstall sasl
+
+Impyla will use the pure-sasl fallback.
+
+
+### 安装说明
+
+由于在我们的服务器上面安装 sasl 之后，不明原因，无法使用。
+
+所以使用了纯 python 实现的 pure-sasl 库。
+
+通过  pipenv install 之后，即会自动安装该库，但是，同时会安装 sasl 库。
+
+而 sasl 库用不了，所以安装完成之后手动卸载掉即可:
+
+pipenv uninstall sasl
+
+
+如果有 sasl，impyla 会使用他，如果他找不到 sasl，就会使用 pure-sasl，所以我们要卸载。
---
+++ tornhivedb.py
@@ -0,0 +1,102 @@
+# coding: u8
+
+import logging
+
+from impala.dbapi import connect
+
+
+class Row(dict):
+    def __getattr__(self, name):
+        try:
+            return self[name]
+        except KeyError:
+            raise AttributeError(name)
+
+
+class Connection(object):
+
+    def __init__(self,
+            host='localhost', port=10000, auth_mechanism="PLAIN",
+            zookeeper_hosts_str=None, **kwargs):
+
+        # zookeeper_hosts_str = 'zk1.example.com:2181,zk2.example.com:2182'
+        if zookeeper_hosts_str:
+            host, port = self.get_hs2_server_address(zookeeper_hosts_str)
+
+        self.conn = connect(
+                host=host, port=port,
+                auth_mechanism=auth_mechanism, **kwargs)
+
+    def get(self, sql, sets=None):
+        """
+        sets can be list:
+
+        sets = [
+            'SET mapred.job.name = myjobname',
+            'set mapreduce.job.queuename=root.offline.myhadoop.queue',
+        ]
+
+        or string:
+
+        sets = 'SET mapred.job.name = myjobname'
+        """
+
+        rows = self.query(sql, sets)
+        if not rows:
+            return None
+        elif len(rows) > 1:
+            raise Exception("Multiple rows returned for get() query")
+        else:
+            return rows[0]
+
+    def query(self, sql, sets=None):
+        cursor = self.set_hiveconf_and_get_cursor(sets)
+        self._execute(cursor, sql)
+        column_names = [i[0] for i in cursor.description]
+        result = [Row(zip(column_names, row)) for row in cursor]
+        self.close(cursor)
+        return result
+
+    def execute(self, sql, sets=None):
+        cursor = self.set_hiveconf_and_get_cursor(sets)
+        self._execute(cursor, sql)
+        self.close(cursor)
+
+    def _execute(self, cursor, sql):
+        try:
+            return cursor.execute(sql)
+        except Exception as e:
+            self.close(self.conn)
+            logging.error(sql)
+            raise(e)
+
+    def set_hiveconf_and_get_cursor(self, sets=None):
+        sets = sets or []
+        if not isinstance(sets, list):
+            sets = [sets]
+
+        cursor = self.conn.cursor()
+        for set_statement in sets:
+            self._execute(cursor, set_statement)
+        return cursor
+
+    def close(self, what):
+        try:
+            what.close()
+        except:
+            pass
+
+    def __del__(self):
+        self.close(self.conn)
+
+    def get_hs2_server_address(self, zookeeper_hosts_str):
+        import random
+        from kazoo.client import KazooClient
+
+        logging.basicConfig()
+        zk = KazooClient(hosts=zookeeper_hosts_str, read_only=True)
+        zk.start()
+
+        address = random.choice(zk.get_children('/hiveserver2'))
+        host, port = address.split(';')[0].split('=')[-1].split(':')
+        return host, int(port)
