4262357388f0368219881acba0b4f1bff86e5ecc
shuji <bh2@qq.com>
2018-04-12 09:21:20 +0800

    update

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+
+setup(
+    name="impyla-torndb",
+    version="0.0.1",
+    description="hiveserver2 impyla client used like torndb api",
+    author="finn",
+    url="https://github.com/Shu-Ji",
+    py_modules=['tornhivedb'],
+)
